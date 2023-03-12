63b2c6879b5e57458584ef05572c8c5d715ccf3a
MikalaiKholin <kholin.mikalai@gmail.com>
2023-03-12 23:52:37 +0300

    feat : task 1 complite

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+
+        System.out.printf("Sum is %d%n", sum);
+        System.out.printf("Sum is %d%n", Arrays.stream(array).sum());
+
+        System.out.println("=========================================");
+
+
     }
 }
